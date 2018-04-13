c163783fc005f04a59bf988b9f7123745f01a36a
AdemHodzic <athos.blade@gmail.com>
2018-04-13 10:32:41 +0200

    Greska sa jokerom

---
+++ joker/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //1. unijeti kuglice i izracunati joker broj
+    //2. Unijeti listice igrca
+    //3. Ispisati dobitke
+    return 0;
+}
